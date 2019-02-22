9ff312b042ff00dbccac977bf45e3651212a6e9a
derPiepmatz <derPiepmatz@users.noreply.github.com>
2019-02-22 23:24:06 +0100

    Optimized Speed Reading Script
    - added some comments
    - moved some global variables to local ones
    - made some things super duper better (hope so)

--- js/speedReading.js
+++ js/speedReading.js
@@ -1,40 +1,74 @@
 document.getElementById("inputText").value = sessionStorage.getItem("convertText");
-//keep text that needs to be converted
+document.getElementById("readingSpeed").value = localStorage.getItem("readingSpeed");
+//paste in saved data
 
-var textToConvert;
+//global variables
 var wordArray;
-var play = false;
-currentWord = 0;
-var wordsPerMinute = 300;
-var cycleFunction;
+var currentWord;
+var loopFunction;
+var play;
 
-var wordTimer = 1 / (wordsPerMinute / 60) * 1000;
-
-function convertText() {
-  textToConvert = document.getElementById("inputText").value;
-  sessionStorage.setItem("convertText", textToConvert);
-  isolateWords();
-  play = true;
-  currentWord = 0;
-  clearInterval(cycleFunction);
-  runReader();
+function wordsPerMinute() {
+  if (document.getElementById("readingSpeed").value == "") {
+    //test for an empty reading speed input
+    alert("No reading Speed");
+    return;
+  }
+  let wordsPerMinute = document.getElementById("readingSpeed").value;
+  localStorage.setItem("readingSpeed", wordsPerMinute);
+  //remember reading speed of user
+  return wordsPerMinute;
 }
 
-function isolateWords() {
+function wordTimer(wordsPerMinute) {
+  let wordsPerSecond = (wordsPerMinute / 60);
+  //get words per second
+  let secondsPerWord = 1 / wordsPerSecond;
+  //get time for each word in seconds
+  let milisecondsPerWord = secondsPerWord * 1000;
+  //get time for each word in miliseconds
+  return milisecondsPerWord;
+}
+
+function convertText() {
+  let textToConvert = document.getElementById("inputText").value;
+  sessionStorage.setItem("convertText", textToConvert);
+  //keep text that needs to be converted for a session
   wordArray = textToConvert.split(" ");
+
+  clearInterval(loopFunction);
+
+  play = true;
+  currentWord = 0;
+  loopFunction = setInterval(runReader, wordTimer(wordsPerMinute()));
+
+  document.getElementById("playPauseButton").focus();
+  //focuses the button to pause via space
+}
+
+function displayWord() {
+  document.getElementById("outputText").innerHTML = wordArray[currentWord];
+  currentWord++;
 }
 
 function runReader() {
-  if(play) {
-    cycleFunction = setInterval(outputWord, wordTimer);
+  displayWord();
+
+  if(currentWord >= wordArray.length) {
+    //cancels reader when text is finished
+    clearInterval(loopFunction);
   }
 }
 
-function outputWord() {
-  document.getElementById("outputText").innerHTML = wordArray[currentWord];
-  console.log(wordArray[currentWord]);
-  currentWord++;
-  if(currentWord >= wordArray.length) {
-    clearInterval(cycleFunction);
+function playPauseReader() {
+  if (play) {
+    //pauses the reader
+    play = false;
+    clearInterval(loopFunction);
+  }
+  else {
+    //continues the reader
+    play = true;
+    loopFunction = setInterval(runReader, wordTimer(wordsPerMinute()));
   }
 }
